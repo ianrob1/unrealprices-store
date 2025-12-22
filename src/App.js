e0eb721996d719c5fba1f0ecf7ebcda1e98ebff6
Ian Robinson <ianrobinson@Ians-MacBook-Pro.local>
2025-12-22 10:06:15 -0500

    “update”

--- src/App.js
+++ src/App.js
@@ -262,7 +262,7 @@ const ProductCard = ({ product, onToggleSave, isSaved }) => {
   </div>
 </div>
   <button
-  onClick={(e) => e.stopPropagation()} 
+   
   className="bg-gray-900 text-white px-4 md:px-6 py-2 md:py-2.5 rounded-md text-xs md:text-sm font-semibold hover:bg-gray-800 transition-colors duration-200 whitespace-nowrap flex-shrink-0">
     Buy Now
   </button>
